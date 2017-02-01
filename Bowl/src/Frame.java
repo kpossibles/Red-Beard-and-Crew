da8eb033e4810b9e5841c9a8802d684423ad7045
beckerej <beckerej@ceas-942-mac03.cae.uwm.edu>
2017-02-01 14:31:24 -0600

    test

--- Bowl/src/Frame.java
+++ Bowl/src/Frame.java
@@ -3,7 +3,9 @@ public class Frame
 {
 	private boolean _isStrike = false;
 	private boolean _isSpare = false;
-	private int _score = 0;
+	private boolean _isThrowable1 = true;
+	private boolean _isThrowable2 = true;
+	private int _pinsDown = 0;
 	private int _throw1 = 0;
 	private int _throw2 = 0;
 	
@@ -16,44 +18,46 @@ public class Frame
 	
 	public int getThrow1(){ return _throw1; }
 	public int getThrow2(){ return _throw2; }
+	public boolean canThrow1(){ return _isThrowable1; }
+	public boolean canThrow2(){ return _isThrowable2; }
 	public boolean getStrike(){ return _isStrike; }
 	public boolean getSpare(){ return _isSpare; }
-	public int getScore(){ return _score; }
-	public void setScore(int s){ _score = s; }
+	public int getpinsDown(){ return _pinsDown; }
+	public void setPinsDown(int s){ _pinsDown = s; }
 	
 	/* === MUTATORS === */
 	
 	/**
-	 * 
 	 * @param t1 - pins on throw1
 	 * @return true on success
 	 */
 	public boolean throw1(int t1){
 		_throw1 = t1;
-		if(_throw1 == 10){ _isStrike = true; }
+		if(_throw1 == 10){ _isStrike = true; _isThrowable2 = false; }
 		else if(_throw1 >= 0 && _throw1 < 10){ _isStrike = false; }
 		else{ return false; }
-		setScore(_throw1);
+		setPinsDown(_throw1);
 		System.out.println("THROW1: " + _throw1);
+		_isThrowable1 = false;
 		return true;
 	}
 	
 	/**
-	 * 
 	 * @param t2 - pins on throw2
 	 * @return true on success
 	 */
 	public boolean throw2(int t2){
 		_throw2 = t2;
-		if(_throw1 == 10){ throw new IllegalStateException("ERROR: Not allowed to throw2 after throw1 is a strike.");}
+		if(_throw1 == 10 || _isThrowable2 == false){ throw new IllegalStateException("ERROR: Not allowed to throw2 after throw1 is a strike.");}
 		int addedThrows = _throw1 + _throw2;
-		setScore(addedThrows);
+		setPinsDown(addedThrows);
 		if(addedThrows == 10){ _isSpare = true;}
 		else if(addedThrows >= 0 || addedThrows < 10){ _isSpare = false; }
 		else{ return false; }
-		setScore(addedThrows);
+		setPinsDown(addedThrows);
 		System.out.println("THROW2: " + _throw2);
 		System.out.println("TOTAL THROWS: " + addedThrows);
+		_isThrowable2 = false;
 		return true;
 	}
 	
--- Bowl/src/ScoreSheet.java
+++ Bowl/src/ScoreSheet.java
@@ -1,4 +1,57 @@
 
 public class ScoreSheet {
-
+	private Frame _frames[] = new Frame[10];
+	private int _index = 0;
+	private int _score;
+	
+	public ScoreSheet(){
+		
+	}
+	
+	/**
+	 * advance in the index by 1
+	 * add to the score
+	 * 
+	 * @param pD - pins down
+	 */
+	public void advanceThrow(int pD)
+	{
+		if(pD>10 || pD < 0){ 
+			throw new IllegalStateException("ERROR: Pins must be between 0 and 10."); 
+		}
+		
+		if(_index > 10){
+			throw new IllegalStateException("ERROR: Cannot be over 10 frames");
+		}
+		
+		if(_frames[_index].canThrow1()){
+			_frames[_index].throw1(pD);
+			updateScore(_frames[_index].getThrow1());
+		}
+		else if(_frames[_index].canThrow2()){
+			_frames[_index].throw2(pD);
+			updateScore(_frames[_index].getThrow2());
+			_index++;
+		}
+		else{
+			
+		}
+	}
+	
+	private void previousScores()
+	{
+		
+	}
+	
+	private void updateScore(int pD){
+		_score += pD;
+	}
+	
+	public int getScore(){
+		return _score;
+	}
+	
+	public Frame getFrame(int index){
+		return _frames[index];
+	}
 }
