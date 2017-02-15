bd35ca766e78dd5acba75dd4b7d40d29b1c44795
beckerej <beckerej@ceas-942-mac04.cae.uwm.edu>
2017-02-15 13:34:44 -0600

    SKELETON FOR ACCOUNT DUN

--- ATM Project/src/Account.java
+++ ATM Project/src/Account.java
@@ -1,4 +1,31 @@
 
 public class Account {
-
+	private int accountNumber;
+	private int pinCode;
+	private double balance;
+	
+	public Account(){
+		
+	}
+	
+	public Account(int aN, int pC, int b){
+		accountNumber = aN;
+		pinCode = pC;
+		balance = b;
+	}
+	
+	public boolean validate(){
+		
+		return true;
+	}
+	
+	public boolean deposit(Card c, int pin, int dep){
+		
+		return true;
+	}
+	
+	public boolean withdraw(Card c, int pin, int dep){
+		
+		return true;
+	}
 }
