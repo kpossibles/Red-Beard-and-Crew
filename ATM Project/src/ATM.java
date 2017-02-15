ed76da5ecc0d65b5305b9dcc807a0797d0273208
beckerej <beckerej@ceas-942-mac04.cae.uwm.edu>
2017-02-15 13:55:17 -0600

    I HATE THIS GUY TO MY LEFT

--- ATM Project/src/ATM.java
+++ ATM Project/src/ATM.java
@@ -1,4 +1,20 @@
 
-public class ATM {
-
+public class ATM 
+{
+	public ATM(){
+		
+	}
+	
+	public void start(){
+	}
+	
+	private boolean withdraw(){
+		
+		return true;
+	}
+	
+	private boolean deposit(){
+		
+		return true;
+	}
 }
--- ATM Project/src/Bank.java
+++ ATM Project/src/Bank.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 
 public class Bank {
-
+	ArrayList<Account> accounts = new ArrayList<Account>();
+	
+	public Bank(){
+		
+	}
+	
+	public boolean withdraw(){
+		
+		return true;
+	}
+	
+	public boolean deposit(){
+		
+		return true;
+	}
 }
