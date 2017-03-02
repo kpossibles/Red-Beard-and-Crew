8ff99d0505e6b3b602d30fed4714b5daf4f268f3
malio <malio@DESKTOP-IK6G0GJ>
2017-03-02 13:38:46 -0600

    Added methods for classes that we don't need yet.

--- Chrono Timer/src/GroupTimed.java
+++ Chrono Timer/src/GroupTimed.java
@@ -1,4 +1,34 @@
 
 public class GroupTimed extends Event {
+
+	@Override
+	public void trigger(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void discard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeRacer(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dnf() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setRun(Run _run) {
+		// TODO Auto-generated method stub
+		
+	}
 //not used in sprint 1
 }
