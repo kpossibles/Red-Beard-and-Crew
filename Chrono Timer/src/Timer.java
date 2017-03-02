2773b3d3157bf3c6d45341b89f4324c3f4355e3e
Evan <Evan@10.20.62.244>
2017-03-02 10:48:17 -0600

    Finished Timer

--- Chrono Timer/src/Timer.java
+++ Chrono Timer/src/Timer.java
@@ -1,19 +1,38 @@
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.*;
+//import java.sql.Timestamp;
+//import java.text.SimpleDateFormat;
+import java.time.Instant; // EB - Solution for epoch (linux) time in millis
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Timer {
-	private Date timer;
-	private SimpleDateFormat sdf = new SimpleDateFormat("sssss.SS");
+	//private Date timer;
+	//private SimpleDateFormat sdf = new SimpleDateFormat("sssss.SS");
+	private long unixTimestamp;
 	
-	public Timer(){
-		timer = new Date();
-		
+	/**
+	 * Timer() will start the time now and set it to unix time
+	 */
+	public Timer(){ 
+		unixTimestamp = Instant.now().getEpochSecond();
 	}
+	
+	/**
+	 * Sets time using a string in the format "HH:MM:SS.X"
+	 * @param number in format "HH:MM:SS.X"
+	 * @return unixTimestamp time equivalent on arbitrary date  
+	 */
 	public void setTime(String number){
-		timer = Timestamp.valueOf(number);
+		String tempDate = "Jul 7 1996 " + number + " UTC"; // Set to random day for math reasons, must be constant.
+        DateTimeFormatter dtf  = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm:ss.SSS zzz");
+        ZonedDateTime     zdt  = ZonedDateTime.parse(tempDate,dtf);        
+        unixTimestamp = zdt.toInstant().toEpochMilli();
 	}
-	public String getTime(){
-		return sdf.format(timer);
+	
+	/**
+	 * 
+	 * @return current time
+	 */
+	public long getTime(){
+		return unixTimestamp;
 	}
 }
