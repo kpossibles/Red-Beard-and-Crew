fb57f7176022e68519584732348538016a6f837c
malio <malio@DESKTOP-IK6G0GJ>
2017-03-08 14:09:53 -0600

    Test

--- Lab7/src/Employee.java
+++ Lab7/src/Employee.java
@@ -1,4 +1,27 @@
 
 public class Employee {
+	String fname;
+	String lname;
+	String dept;
+	String phone;
+	
+	public Employee(String _fname, String _lname, String _dept, String _phone){
+		fname = _fname;
+		lname = _lname;
+		dept = _dept;
+		phone = _phone;
+	}
+	
+	public Employee(String json){
+		//TODO
+	}
+	
+	public String toJSON(){
+		return "BLARG";
+	}
+	
+	public String toString(){
+		return "TODO";
+	}
 
 }
