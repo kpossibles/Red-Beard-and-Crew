272a841d4963b5e5de69cc182be1270db46a9b97
malio <malio@DESKTOP-IK6G0GJ>
2017-03-08 13:51:05 -0600

    Added Employee

---
+++ JSON-LAB/src/Employee.java
@@ -0,0 +1,27 @@
+
+public class Employee {
+	String fname;
+	String lname;
+	String dept;
+	String phone;
+	
+	public Employee(String _fname, String _lname, String _dept, String _phone){
+		fname = _fname;
+		lname = _lname;
+		dept = _dept;
+		phone = _phone;
+	}
+	
+	public Employee(String json){
+		//TODO
+	}
+	
+	public String toJSON(){
+		return "TODO";
+	}
+	
+	public String toString(){
+		return "TODO";
+	}
+
+}
